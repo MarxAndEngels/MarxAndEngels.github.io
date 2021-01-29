b49e8148db9165214f238f1bcf3ceeb46792fdba
MarxAndEngels <banana13373228@gmail.com>
2021-01-29 13:31:59 +0300

    1
    1

--- dr/gamecl.js
+++ dr/gamecl.js
@@ -11,10 +11,7 @@ mus.src= "audio/осень.mp3";
 
 function start() {
      const startTime= Date.now();
-
      display.textContent = time;
-
-    display.textContent= time;
     button.onclick = () => counter.textContent = clicks++
     mus.play();
 
@@ -26,7 +23,6 @@ function start() {
 
     const timeout = setTimeout( ()=> {
         button.onclick = null;
-        display.textContent = 'Game Over';
         location.reload();
         alert("Счет = "+ counter.textContent ) ;
 
