9b83e9a52896278068fc66231bde12f177e669fb
MarxAndEngels <banana13373228@gmail.com>
2021-02-02 12:59:11 +0300

    1
    1

--- dr/main.js
+++ dr/main.js
@@ -160,3 +160,37 @@ class Clock {
   footer.addEventListener('click', () => {
     scrollTo(button);
   })
+
+function inter(text,ele, speed){
+  this.text = text;
+  this.ele = document.querySelector(ele);
+
+  let txt = this.text.split("");
+    var interval = setInterval(function(){
+      if(!txt[0]){
+        return clearInterval(interval);
+     };
+      this.ele.innerHTML += txt.shift();
+   }, speed != undefined ? speed : 80 );
+}
+
+var str = document.getElementById('mactext').innerHTML.toString();
+var i=0;
+document.getElementById('mactext').innerHTML = "";
+setTimeout(function () {
+    var se = setInterval(function() {
+        document.getElementById('mactext').innerHTML = str.slice(0, i) + "|";
+        if (i == str.length) {
+            clearInterval(se);
+        }
+        i++;
+        console.log(i);
+    }, 60);
+});
+
+
+
+
+
+
+
