4ab0d05013255e74587c52a974a92eb01fc7d341
MarxAndEngels <banana13373228@gmail.com>
2021-02-16 16:57:31 +0300

    1
    1

--- dr/main.js
+++ dr/main.js
@@ -213,6 +213,22 @@ range.onchange = function(){
 }
 
 
+if (document.documentElement.clientWidth < 768) {
+ removeAtribl(mactextJS1);
+ removeAtribl(mactextJS2);
+ removeAtribl(mactextJS3);
+ removeAtribl(mactextJS4);
+ removeAtribl(mactextJS5);
+ removeAtribl(mactextJS6);
+ removeAtribl(mactextJS7);
+ removeAtribl(mactextJS8);
+ removeAtribl(mactextJS9);
+ removeAtribl(mactextJS10);
+}
+function removeAtribl(el ){
+el.removeAttribute('data-wow-delay');
+}
+
 
 
 
